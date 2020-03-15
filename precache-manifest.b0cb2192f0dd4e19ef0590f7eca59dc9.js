904a8cda5598cd36f8066bdbf0405ea127378e34
Jason <jbugallo@uci.edu>
2020-03-15 13:22:08 -0700

    Updates

---
+++ precache-manifest.b0cb2192f0dd4e19ef0590f7eca59dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faebd09a2eb27800513797aa5df4eade",
+    "url": "/learning-react-todo-list/index.html"
+  },
+  {
+    "revision": "686e8622eaa216d9f5b3",
+    "url": "/learning-react-todo-list/static/css/main.4b52c219.chunk.css"
+  },
+  {
+    "revision": "19d63b5a790a439e7755",
+    "url": "/learning-react-todo-list/static/js/2.c86a418b.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/learning-react-todo-list/static/js/2.c86a418b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686e8622eaa216d9f5b3",
+    "url": "/learning-react-todo-list/static/js/main.f643f32f.chunk.js"
+  },
+  {
+    "revision": "315121c9f7d3fad4f1de",
+    "url": "/learning-react-todo-list/static/js/runtime-main.24d34a77.js"
+  }
+]);
